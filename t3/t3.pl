a69201dab9326b1115aee1b4d83a08111dcaf673
Bernardo Luis Hamester <37083276+bernardox0L@users.noreply.github.com>
2018-04-11 16:12:20 -0300

    Create t3.pl

---
+++ t3/t3.pl
@@ -0,0 +1,16 @@
+% 1. Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
+zeroInit(L):- L=[H|_], H=:=0.
+
+% 2. Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos. Resolva este exercício sem usar um predicado auxiliar.
+has5l(L):- L=[_,_,_,_,_].
+
+% 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
+hasN(L,N):- length(L, N).
+
+% 4. Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0. Exemplo de uso:
+potN0(0,[1]).
+potN0(N,L):- N>=0,L=[H|T],H is 2^N, N1 is N-1, potN0(N1,T).
+
+% 5. Defina um predicado zipmult(L1,L2,L3), de forma que cada elemento da lista L3 seja o produto dos elementos de L1 e L2 na mesma posição do elemento de L3. Exemplo:
+zipmult([],[],[]).
+zipmult(L1,L2,L3):- L1=[H1|T1],L2=[H2|T2],L3=[H3|T3], H3 is H1*H2, zipmult(T1,T2,T3).
