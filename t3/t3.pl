942601e9240d20c11e3fe22de6e76e4c10948967
Bernardo Luis Hamester <37083276+bernardox0L@users.noreply.github.com>
2018-04-15 17:06:09 -0300

    Adicionar questão 6
    Add q6

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,4 +1,4 @@
-% 1. Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
+﻿% 1. Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
 zeroInit(L):- L=[H|_], H=:=0.
 
 % 2. Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos. Resolva este exercício sem usar um predicado auxiliar.
@@ -14,3 +14,14 @@ potN0(N,L):- N>=0,L=[H|T],H is 2^N, N1 is N-1, potN0(N1,T).
 % 5. Defina um predicado zipmult(L1,L2,L3), de forma que cada elemento da lista L3 seja o produto dos elementos de L1 e L2 na mesma posição do elemento de L3. Exemplo:
 zipmult([],[],[]).
 zipmult(L1,L2,L3):- L1=[H1|T1],L2=[H2|T2],L3=[H3|T3], H3 is H1*H2, zipmult(T1,T2,T3).
+
+% 6. Defina um predicado potencias(N,L), de forma que L seja uma lista com as N primeiras potências de 2, sendo a primeira 2^0 e assim por diante, conforme o exemplo abaixo:
+potencias(N,L):- potencias2(0,N,L).
+    
+potencias2(N,N,[]).
+potencias2(X,N,L):- 
+    X=<N, 
+    L=[H|T], 
+    H is 2^X, 
+    XI is X+1, 
+    potencias2(XI,N,T).
