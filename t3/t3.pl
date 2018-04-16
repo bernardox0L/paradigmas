874bef72960efa0aa7e601c736e8b7dd015876c7
Bernardo Luis Hamester <37083276+bernardox0L@users.noreply.github.com>
2018-04-16 14:43:07 -0300

    Corrigir questão 7

--- t3/t3.pl
+++ t3/t3.pl
@@ -26,18 +26,13 @@ potencias2(X,N,L):-
     XI is X+1, 
     potencias2(XI,N,T).
 
-% *7. Defina um predicado positivos(L1,L2), de forma que L2 seja uma lista só com os elementos positivos de L1, conforme o exemplo abaixo:
-positivos([],_).
+% 7. Defina um predicado positivos(L1,L2), de forma que L2 seja uma lista só com os elementos positivos de L1, conforme o exemplo abaixo:
+positivos([],[]).
 positivos(L1,L2):- 
     L1=[H1|T1],
-    L2=[H2,T2], 
-    H1>0, 
-    H2 is H1, 
-    positivos(T1,T2).
-
-positivos(L1,L2) :-
-    L1 = [_|T1],
-    positivos(T1, L2).
+    L2=[H2|T2], 
+    (H1>0 -> H2 is H1, T3 = T2; T3 = L2), 
+    positivos(T1,T3).
 
 % 8. Considere que L1 e L2 sejam permutações de uma lista de elementos distintos, sem repetições. Sabendo disso, defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. Exemplo de uso:
 mesmaPosicao(A,[A|_],[A|_]).
